e522ef5ab08dc1bbb791fc6b377f99dc63a5cdb3
shubham9435 <85381268+shubham9435@users.noreply.github.com>
2021-06-14 22:36:11 +0530

    Add files via upload

---
+++ f1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n) for (int i = 2; i <= sqrt(n); i++)
+{
+    if (n % i == 0)
+    {
+        return false;
+    }
+    return true;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (isPrime(i))
+    {
+        cout << i << endl;
+    }
+}
